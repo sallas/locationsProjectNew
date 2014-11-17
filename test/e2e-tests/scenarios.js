ad06e948758c3ac04ba1ebac6ec9061089d4d6bc
sallas <tbc105@hotmail.com>
2014-11-17 22:15:00 +0100

    Change the project to allow for more advanced protractor tests and added scenarios using this facts.

--- test/e2e-tests/scenarios.js
+++ test/e2e-tests/scenarios.js
@@ -1,20 +1,23 @@
 'use strict';
-
+global.TEST_DATABASE = "mongodb://localhost/TestDataBase_xx1243";
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
+var db = require('../../server/model/db');
+var mongoose = require("mongoose");
+var Location = mongoose.model("Location");
 
 describe('my app', function () {
 
     browser.get('/');
 
     it('should automatically redirect to /view1 when location hash/fragment is empty', function () {
-        expect(browser.getLocationAbsUrl()).toMatch("/view1");
+        expect(browser.getLocationAbsUrl()).toMatch("/home");
     });
 
 
     describe('view1', function () {
 
         beforeEach(function () {
-            browser.get('#/view1');
+            browser.get('#/home');
         });
 
 
@@ -25,18 +28,47 @@ describe('my app', function () {
 
     });
 
+    describe('viewLocations', function () {
 
-    describe('view2', function () {
-
-        beforeEach(function () {
-            browser.get('#/view2');
+        beforeEach(function (done) {
+            browser.get('#/locations');
+            Location.remove({}, function () {
+                var array = [
+                    {
+                        "Name": "Bad location",
+                        "Description": "molestie. Sed id risus quis diam luctus lobortis. Class aptent taciti sociosqu ad",
+                        "Rating": 3,
+                        "country": "Bangladesh",
+                        "cty": "Brampton",
+                        "zip": "5419",
+                        "street": "720-6932 Fames Avenue",
+                        "gps": "17.11372, -100.30735"
+                    },
+                    {
+                        "Name": "Good location",
+                        "Description": "Aenean egestas hendrerit neque. In ornare sagittis felis. Donec tempor, est ac mattis",
+                        "Rating": 4,
+                        "country": "Japan",
+                        "cty": "Oppido Mamertina",
+                        "zip": "5340",
+                        "street": "2188 Venenatis St.",
+                        "gps": "-54.38124, -62.16094"
+                    }
+                ];
+                Location.create(array, function (err) {
+                    done();
+                });
+            });
         });
 
-
-        it('should render view2 when user navigates to /view2', function () {
-            expect(element.all(by.css('[ng-view] p')).first().getText()).
-                toMatch(/partial for view 2/);
+        it('should render view1 when user navigates to /view1', function () {
+            var allLocations = element.all(by.repeater('location in locations'));
+            expect(allLocations.count()).toEqual(2);
+            var locationName = element(by.binding('location.Name')).getText();
+            expect(locationName).toEqual('Bad location');
         });
 
     });
+
+
 });
